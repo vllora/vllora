158ff94997d6ab38f535cd590041ccd97c575bc7
Karolis Gudiškis <karolisgud@gmail.com>
2025-10-27 08:34:19 +0200

    chore update vllora

---
+++ Formula/vllora.rb
@@ -0,0 +1,42 @@
+class Vllora < Formula
+  desc "vllora - Multi-provider AI gateway server"
+  homepage "https://github.com/langdb/vllora"
+  version "0.4.0-prerelease-10"  # Update this with your version
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-10/ai-gateway-macos-aarch64"
+      sha256 "5e6ed0da4e008d950d5fc66dc12fe5f1d42165fc9b408cefa7cd4964e77de5dc"  # Run: shasum -a 256 ai-gateway-aarch64
+    else
+      url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-10/ai-gateway-macos-x86_64"
+      sha256 "f31497f451e2546e58aa1e6f58ea3abcb22e57fe27014876a10c1cc29ae160a3"  # Run: shasum -a 256 ai-gateway-x86_64
+    end
+  end
+  on_linux do
+  #   if Hardware::CPU.arm?
+  #     url "https://github.com/langdb/ai-gateway/releases/download/v0.3.2/ai-gateway-linux-aarch64"
+  #     sha256 "f9dbe7dfbe1f7a6a817f0d3a674d54ad07062496e5753106d42d916ef450b7b2"  # Run: shasum -a 256 ai-gateway-aarch64
+  #   else
+      url "https://github.com/langdb/ai-gateway/releases/download/v0.4.0-prerelease-10/ai-gateway-linux-x86_64"
+      sha256 "63c4bc1600ed1c3fdb600d82cb64e481a9b8d2832aa33f1420902b91fcee790a"  # Run: shasum -a 256 ai-gateway-x86_64
+  #   end
+  end
+  def install
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "ai-gateway-macos-aarch64" => "vllora"
+      else
+        bin.install "ai-gateway-macos-x86_64" => "vllora"
+      end
+    elsif OS.linux?
+      bin.install "ai-gateway-linux-x86_64" => "vllora"
+    end
+  end
+  def caveats
+    # <<~EOS
+    #   Server is running on port 8080 and UI can be accessed at http://localhost:8084
+    # EOS
+  end
+  test do
+    system "#{bin}/ai-gateway", "--version"
+  end
+end
